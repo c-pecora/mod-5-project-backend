e10edfd56fef8ed9748c7e2e775e03ecdf216f7b
Carly Pecora <carly@carlypecora.com>
2019-07-30 09:45:51 -0400

    able to render notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,4 +4,10 @@ class NotificationsController < ApplicationController
 		byebug
 	end
 
+	def update
+		@notification = Notification.find(params[:id])
+		@notification.update(read: true)
+		render json: @notification
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_194919) do
+ActiveRecord::Schema.define(version: 2019_07_29_213110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_07_26_194919) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "read", default: false
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
