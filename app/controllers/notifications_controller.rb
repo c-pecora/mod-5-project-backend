24049e777b9fa4524eb345a892acffc51782ca70
Carly Pecora <carly@carlypecora.com>
2019-07-30 17:08:58 -0400

    notifications finally work

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,10 +3,12 @@ class NotificationsController < ApplicationController
 	def create
 		users = User.all.select { |user |user.conversation_ids.include?(params[:convo_id]) }
     	users.each do |user| 
-    		notification = Notification.new(content: "new notification in #{params[:content]}", user_id: user.id)
+    		if user.id != params[:user_id]
+    		notification = Notification.new(content: "new notification in #{params[:content]}", user_id: user.id, read: false)
 		    if notification.save
-			   	ActionCable.server.broadcast 'notifications_channel', NotificationSerializer.new(notification)
+			   	ActionCable.server.broadcast 'notifications_channel', {notification: NotificationSerializer.new(notification), sender_id: params[:user_id]}
 			end
+		end
 	    end
 	    render json: users
 	end
--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -1,4 +1,4 @@
 class NotificationSerializer < ActiveModel::Serializer
-  attributes :id, :content, :created_at, :read
+  attributes :id, :content, :created_at, :read, :user_id
   belongs_to :user
 end
